8421398331f66b48af21a3b977c72c931a57c5c2
erinwangyy <59551284+erinwangyy@users.noreply.github.com>
2020-02-05 14:06:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+//DOM load event
+window.addEventListener("DOMContentLoaded", () => {
+
+    const spotlight = document.querySelector('.spotlight');
+
+    let spotlightSize = 'transparent 100px, rgba(0, 0, 0, 0.85) 100px)';
+
+    window.addEventListener('mousemove', e => updateSpotlight(e));
+
+    window.addEventListener('mousedown', e => {
+
+        spotlightSize = 'transparent 100px, rgba(0, 0, 0, 0.95) 100px)';
+
+        updateSpotlight(e);
+
+    });
+
+    window.addEventListener('mouseup', e => {
+
+        spotlightSize = 'transparent 100px, rgba(0, 0, 0, 0.85) 110px)';
+
+        updateSpotlight(e);
+
+    });
+
+    function updateSpotlight(e) {
+
+        spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${spotlightSize}`;
+
+    }
+});
